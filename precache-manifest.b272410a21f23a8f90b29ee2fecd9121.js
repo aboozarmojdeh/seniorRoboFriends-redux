2091bf945207c4a5863c4c86315e271ee4803a0e
aboozarmojdeh <aboozar.mojdeh@gmail.com>
2020-09-19 15:30:46 -0400

    Updates

---
+++ precache-manifest.b272410a21f23a8f90b29ee2fecd9121.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697721fedf83b4a578b0359372b895bb",
+    "url": "/seniorRoboFriends-redux/index.html"
+  },
+  {
+    "revision": "43e95ed65b3086c502eb",
+    "url": "/seniorRoboFriends-redux/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "52f39825ad42627d676d",
+    "url": "/seniorRoboFriends-redux/static/css/main.cf801fc0.chunk.css"
+  },
+  {
+    "revision": "43e95ed65b3086c502eb",
+    "url": "/seniorRoboFriends-redux/static/js/2.6fbeb504.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/seniorRoboFriends-redux/static/js/2.6fbeb504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52f39825ad42627d676d",
+    "url": "/seniorRoboFriends-redux/static/js/main.6ed6d2bf.chunk.js"
+  },
+  {
+    "revision": "f917d7f7be762e86eb96",
+    "url": "/seniorRoboFriends-redux/static/js/runtime-main.77f9de5c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/seniorRoboFriends-redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
